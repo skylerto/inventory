2f847c0fb17058f0115cf2550293a7336905aa6a
skylerto <skylerclayne@gmail.com>
2015-07-09 11:16:18 -0400

    added coveralls

--- Gemfile
+++ Gemfile
@@ -3,4 +3,4 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in inventory.gemspec
 gemspec
 
-gem "codeclimate-test-reporter", group: :test, require: nil
+gem 'coveralls', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'inventory'
 require 'inventory/bag'
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+
+require 'coveralls'
+Coveralls.wear!
