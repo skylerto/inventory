c536ebda2314d7072a8c5c41a929dbc07bc6abcf
Skyler Layne <skylerclayne@gmail.com>
2015-10-13 08:06:53 -0400

    setup mysql connection

--- lib/inventory/dbconnection.rb
+++ lib/inventory/dbconnection.rb
@@ -4,8 +4,11 @@ require 'active_record'
 ActiveRecord::Base.establish_connection(
     :adapter=> "mysql",
     :host => "localhost",
-    :database=> "inventory"
+   :database=> "inventory"
 )
+# :username => "inv",
+ #   :password => "inventory_password",
+    
 
 class Thing < ActiveRecord::Base
 end
